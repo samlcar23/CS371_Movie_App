d72063fdeb3d82a8718b7b7c7da42535923a7b9e
Andy Hung <andy.o.hung@gmail.com>
2019-04-07 13:56:11 -0400

    Working addRent, addUser, login (use global guid to reference uid),deleteRent.

--- movie-app/src/App.vue
+++ movie-app/src/App.vue
@@ -1,31 +1,74 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+  <v-app>
+    <div id="app">
+      <div id="nav">
+        <v-toolbar app>
+          <!-- <v-toolbar-side-icon
+        v-if="$root.loggedIn"
+        class="hidden-md-and-up"
+        @click.end="drawerToggle()"
+          ></v-toolbar-side-icon>-->
+
+          <v-toolbar-title class="headline text-uppercase">
+            <span class="noBackground" @click="$router.push({ path: `/` })">PremierRent</span>
+          </v-toolbar-title>
+          <v-spacer></v-spacer>
+
+          <v-btn
+            v-if="!$root.loggedIn"
+            @click="$root.loggedIn=true"
+            color="primary"
+            flat
+            large
+            class="mr-2"
+          >
+            <v-avatar size="35px">
+              Log In
+              <v-icon right size="30px" color="#1176c4">fas fa-user</v-icon>
+            </v-avatar>
+          </v-btn>
+          <v-menu s3 v-if="$root.loggedIn">
+            <v-btn slot="activator" color="primary" flat icon large class="mr-2">
+              <v-avatar size="35px">
+                <v-icon round size="30px" color="#1176c4">fas fa-user</v-icon>
+              </v-avatar>
+            </v-btn>
+            <v-list>
+              <v-list-tile>
+                <v-list-tile-action>
+                  <v-btn flat>
+                    <span class="navButtons" @click="$router.push({ path: `/account` })">My Account</span>
+                  </v-btn>
+                </v-list-tile-action>
+              </v-list-tile>
+              <v-list-tile>
+                <v-list-tile-action>
+                  <v-btn flat>
+                    <span class="navButtons" @click="$root.loggedIn=false">Log Out</span>
+                  </v-btn>
+                </v-list-tile-action>
+              </v-list-tile>
+            </v-list>
+          </v-menu>
+        </v-toolbar>
+      </div>
+      <v-content>
+        <router-view/>
+      </v-content>
+
+      <!-- footer -->
+      <v-footer class="text-md-center">
+        <v-spacer></v-spacer>
+        <span class="body-1">&nbsp;Copyright&nbsp;</span>
+        <div>&copy; {{ new Date().getFullYear() }}&nbsp;</div>
+        <span class="body-1">&nbsp;|&nbsp;</span>
+        <span class="body-1">PremierRent.&nbsp;</span>
+        <span class="body-1">All Rights Reserved.</span>
+        <v-spacer></v-spacer>
+      </v-footer>
     </div>
-    <router-view/>
-  </div>
+  </v-app>
 </template>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
--- movie-app/src/views/Home.vue
+++ movie-app/src/views/Home.vue
@@ -1,18 +1,90 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <v-breadcrumbs class="ml-4" :items="navPath" divider=">"></v-breadcrumbs>
+    <v-container fluid grid-list-md class="py-4 mx-0">
+      <v-layout row wrap justify-center>
+        <!-- <p>{{nowPlayingMovies[0]}}</p> -->
+        <template v-for="(movie, index) in $root.movieList">
+          <v-flex :key="index" xs11 sm5 md2 lg2 xl2>
+            <v-card dark color="primary" @click="movieDetails(movie)">
+              <v-img :src="movie.poster"></v-img>
+              <v-card-text>
+                {{movie.title}}
+                <WatchCounter :movieId="movie.id"></WatchCounter>
+              </v-card-text>
+            </v-card>
+          </v-flex>
+        </template>
+      </v-layout>
+      <v-layout row wrap justify-center>
+        <v-flex>
+          <MoviePopup :movie="selectedMovie"></MoviePopup>
+        </v-flex>
+      </v-layout>
+    </v-container>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
+import WatchCounter from "../components/WatchCounter.vue";
+import MoviePopup from "../components/MoviePopup.vue";
+
 
 export default {
-  name: 'home',
+  mounted() {
+    if (this.$root.movieList.length <= 0) this.getNowPlaying();
+  },
   components: {
-    HelloWorld
+    WatchCounter,
+    MoviePopup
+  },
+  data() {
+    return {
+      nowPlaying: [],
+      selectedMovie: {},
+      navPath: [
+      {
+        text: "Home",
+        disabled: true,
+        to: "/"
+      }
+    ]
+    };
+  },
+  methods: {
+    getNowPlaying() {
+      fetch(
+        `https://api.themoviedb.org/3/movie/now_playing?api_key=bbd53a03bcbbff4022afbfd11ffa06a3&language=en-US&page=1`
+      )
+        .then(r => r.json())
+        .then(u => {
+          this.nowPlaying = u.results;
+        })
+        .then(p => {
+          this.nowPlaying.forEach(element => {
+            this.getImages(element);
+          });
+        });
+    },
+    getImages(obj) {
+      this.$root.movieList = [];
+      fetch(`https://image.tmdb.org/t/p/w500${obj.poster_path}`).then(r => {
+        this.$root.movieList.push({
+          ...obj,
+          poster: r.url
+        });
+      });
+    },
+
+    movieDetails(movie) {
+      this.selectedMovie = { ...movie };
+    },
+
   }
-}
+};
 </script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
